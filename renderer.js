a7819af33e72635ac049fa6459c773d66c93f7e1
Junior Dassen Nana <JuniorDassen.Nana@proalpha.com>
2023-07-13 14:59:05 +0200

    Backend-Anfang

--- renderer.js
+++ renderer.js
@@ -1,7 +1,47 @@
-/**
- * This file is loaded via the <script> tag in the index.html file and will
- * be executed in the renderer process for that window. No Node.js APIs are
- * available in this process because `nodeIntegration` is turned off and
- * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
- * to expose Node.js functionality from the main process.
- */
+
+document.addEventListener('DOMContentLoaded', function () {
+    const versionSelect = document.getElementById('version-select');
+    const tableContainer = document.getElementById('table-container');
+
+    versionSelect.addEventListener('change', function () {
+      const selectedVersion = versionSelect.value;
+      if (selectedVersion !== '') {
+        fetch('prepare.json')
+          .then(response => response.json())
+          .then(data => {
+            const downloads = data.padownloads.find(download => download._version === selectedVersion);
+            if (downloads) {
+              const table = document.createElement('table');
+              table.setAttribute('border', '1');
+              table.style.backgroundColor = 'white';
+
+              const headerRow = document.createElement('tr');
+              ['chk', 'name', 'filename', 'destination', 'status'].forEach(headerText => {
+                const th = document.createElement('th');
+                th.textContent = headerText;
+                headerRow.appendChild(th);
+              });
+              table.appendChild(headerRow);
+
+              downloads.download.forEach(download => {
+                const row = document.createElement('tr');
+                ['chk', 'name', 'filename', 'destination', 'status'].forEach(key => {
+                  const td = document.createElement('td');
+                  td.textContent = download[key] || '';
+                  row.appendChild(td);
+                });
+                table.appendChild(row);
+              });
+
+              tableContainer.innerHTML = '';
+              tableContainer.appendChild(table);
+            }
+          })
+          .catch(error => {
+            console.error('Error:', error);
+          });
+      } else {
+        tableContainer.innerHTML = '';
+      }
+    });
+  });
