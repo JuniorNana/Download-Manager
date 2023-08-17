d838ef0af6e27d9e05ff70a2bb041be7d8524f0e
Junior Dassen Nana <JuniorDassen.Nana@proalpha.com>
2023-08-17 10:33:06 +0200

    last_update

--- src/renderer.js
+++ src/renderer.js
@@ -1,10 +1,14 @@
+// Einbinden der benötigten Module
 const { ipcRenderer } = require('electron');
 const fs = require('fs');
 const path = require('path');
 
+// Funktion zum Erzeugen von Ordnern basierend auf der ausgewählten proALPHA-Version
 async function createFoldersFromConfig(basePath, configData, selectedVersion) {
+  // Erstelle eine Map aus den Ordnereinstellungen der Konfigurationsdaten
   const folderMap = new Map(configData.FolderCreation.map(folder => [folder['@id'], folder]));
 
+  // Rekursive Hilfsfunktion zum Erzeugen von Ordnern
   const createFolderRecursive = (folderId, parentPath) => {
     const folder = folderMap.get(folderId);
     if (!folder) return;
@@ -26,16 +30,20 @@ async function createFoldersFromConfig(basePath, configData, selectedVersion) {
     }
   };
 
+  // Beginne mit der Erzeugung von Root-Ordnern
   const rootFolders = configData.FolderCreation.filter(folder => folder['@root'] === '@root@');
   for (const rootFolder of rootFolders) {
     createFolderRecursive(rootFolder['@id'], basePath);
   }
 }
 
+// Event-Listener, der beim Klicken des "create-button" ausgelöst wird
 document.getElementById('create-button').addEventListener('click', function () {
+  // Ausgewählten Ordner und Version abrufen
   const selectedFolder = document.getElementById('folder-picker').value;
   const selectedVersion = document.getElementById('version-select').value;
 
+  // Überprüfe, ob der ausgewählte Pfad ein Ordner ist
   fs.lstat(selectedFolder, (err, stats) => {
     if (err) {
       console.error('Error accessing selected folder:', err);
@@ -47,6 +55,7 @@ document.getElementById('create-button').addEventListener('click', function () {
       return;
     }
 
+    // Lese die Konfigurationsdatei
     fs.readFile(path.join(__dirname, 'config.json'), 'utf8', (err, data) => {
       if (err) {
         console.error('Error reading config file:', err);
@@ -54,6 +63,7 @@ document.getElementById('create-button').addEventListener('click', function () {
         return;
       }
 
+      // Parse die Konfigurationsdatei
       let configData;
       try {
         configData = JSON.parse(data);
@@ -63,6 +73,7 @@ document.getElementById('create-button').addEventListener('click', function () {
         return;
       }
 
+      // Versuche, Ordner zu erstellen
       try {
         createFoldersFromConfig(selectedFolder, configData, selectedVersion);
         ipcRenderer.send('folder-creation-result', true);
@@ -74,6 +85,7 @@ document.getElementById('create-button').addEventListener('click', function () {
   });
 });
 
+// Behandle das Ergebnis des Ordnererstellungsprozesses
 ipcRenderer.on('folder-creation-result', (event, success) => {
   if (success) {
     alert('Ordner wurden erfolgreich erstellt!');
@@ -82,18 +94,22 @@ ipcRenderer.on('folder-creation-result', (event, success) => {
   }
 });
 
-
+// Referenzen auf HTML-Elemente
 const versionSelect = document.getElementById('version-select');
 const tableContainer = document.getElementById('table-container');
 
+// Event-Listener für das Laden des Dokuments
 document.addEventListener('DOMContentLoaded', function() {
+  // Erstelle eine Tabelle, wenn das Dokument geladen wird
   const table = document.createElement('table');
   table.setAttribute('border', '1');
   table.style.backgroundColor = 'white';
   tableContainer.appendChild(table);
 });
 
+// Event-Listener für Änderungen im Version-Auswahl-Dropdown
 versionSelect.addEventListener('change', function () {
+  // Ausgewählte Version abrufen
   const selectedVersion = versionSelect.value;
   if (selectedVersion !== '') {
     fetch('../prepare.json')
@@ -101,10 +117,12 @@ versionSelect.addEventListener('change', function () {
       .then(data => {
         const downloads = data.padownloads.find(download => download._version === selectedVersion);
         if (downloads) {
+          // Erstelle eine Tabelle basierend auf der ausgewählten Version
           const table = document.createElement('table');
           table.setAttribute('border', '1');
           table.style.backgroundColor = 'white';
 
+          // Erstelle Tabellenheader
           const headerRow = document.createElement('tr');
           ['chk', '_name', 'filename', 'destination', 'status'].forEach(headerText => {
             const th = document.createElement('th');
@@ -113,6 +131,7 @@ versionSelect.addEventListener('change', function () {
           });
           table.appendChild(headerRow);
 
+          // Befülle die Tabelle mit Daten
           downloads.download.forEach(download => {
             const row = document.createElement('tr');
             ['chk', '_name', 'filename', 'destination', 'status'].forEach(key => {
@@ -129,6 +148,7 @@ versionSelect.addEventListener('change', function () {
             table.appendChild(row);
           });
 
+          // Setze den Tabelleninhalt zurück und füge die neue Tabelle hinzu
           tableContainer.innerHTML = '';
           tableContainer.appendChild(table);
         }
@@ -140,14 +160,3 @@ versionSelect.addEventListener('change', function () {
     tableContainer.innerHTML = '';
   }
 });
-
-
-
-
-
-
-
-
-
-
-
